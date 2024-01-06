e20dbd7db2e2664758a83680a69c1067b516da93
Divyaa Sharmaa <shdivya1310@gmail.com>
2024-01-06 23:44:27 +0530

    Bank Management System

--- bank-management-system/src/SignUpPage/SignUp.js
+++ bank-management-system/src/SignUpPage/SignUp.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bank_icon from '../Images/bank-icon-1.jpg';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function SignUp() {
+
+  let navigate = useNavigate();
+
+  const[banker, setBanker] = useState({
+    name: "",
+    father_name:"",
+    dob:"",
+    gender:"",
+    email: "",
+    marital_status: "",
+    address: "",
+    state: "",
+    city: "",
+    pincode: ""
+  });
+
+  const {name,father_name,dob,gender, email,marital_status,address,state,city,pincode} = banker;
+
+  const onInputChange = (e) => {
+    setBanker({...banker,[e.target.name]:e.target.value})
+  }
+
+  const onClick = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/banker",banker)
+    // navigate("/additionalDetails");
+}
+
   return (
     <div className='container'>
         <div className="row">
@@ -20,34 +49,40 @@ export default function SignUp() {
                 <div className="row mb-3">
                    <label for="name" className="col-sm-4 col-form-label"><b>Name:</b></label>
                    <div className="col-sm-7">
-                     <input type="name" className="form-control border-dark" id="name" placeholder=""/>
+                     <input name="name" type="name" className="form-control border-dark" 
+                     id="name" value={name} onChange={(e) => {onInputChange(e)}} placeholder=""/>
                    </div>
                 </div>
                 <div className="row mb-3">
                    <label for="Fname" className="col-sm-4 col-form-label"><b>Father's Name:</b></label>
                    <div className="col-sm-7">
-                     <input type="Fname" className="form-control border-dark" id="Fname" placeholder=""/>
+                     <input name='father_name' type="Fname" className="form-control border-dark" 
+                     value={father_name} id="Fname" placeholder="" onChange={(e) => {onInputChange(e)}}/>
                    </div>
                 </div>              
                 <div className="row mb-3">
                    <label htmlFor="dob" className="col-sm-4 col-form-label"><b>Date of Birth:</b></label>
                    <div className="col-sm-7">
-                     <input type="date" className="form-control border-dark" id="dob" placeholder=""/>
+                     <input name='dob' type="date" className="form-control border-dark" 
+                     value={dob} id="dob" onChange={(e) => {onInputChange(e)}} placeholder=""/>
                    </div>
                 </div>           
                 <div className="row mb-3">
                    <label htmlFor="gender" className="col-sm-4 col-form-label"><b>Gender:</b></label>
                    <div className="col-sm-6">
                      <div className="form-check form-check-inline">
-                       <input className="form-check-input" type="radio" name="gender" id="male" value="male" />
+                       <input  className="form-check-input" type="radio" name="gender" id="male" 
+                       value={gender} onChange={(e) => {onInputChange(e)}}/>
                        <label className="form-check-label" htmlFor="male">Male</label>
                      </div>
                      <div className="form-check form-check-inline">
-                       <input className="form-check-input" type="radio" name="gender" id="female" value="female" />
+                       <input className="form-check-input" type="radio" name="gender" id="female" 
+                       value={gender} onChange={(e) => {onInputChange(e)}}/>
                        <label className="form-check-label" htmlFor="female">Female</label>
                      </div>
                      <div className="form-check form-check-inline">
-                       <input className="form-check-input" type="radio" name="gender" id="other" value="other" />
+                       <input className="form-check-input" type="radio" name="gender" id="other" 
+                       value={gender} onChange={(e) => {onInputChange(e)}}/>
                        <label className="form-check-label" htmlFor="other">Other</label>
                      </div>
                    </div>
@@ -55,51 +90,59 @@ export default function SignUp() {
                 <div className="row mb-3">
                    <label htmlFor="email" className="col-sm-4 col-form-label"><b>Email:</b></label>
                    <div className="col-sm-7">
-                     <input type="email" className="form-control border-dark" id="email" placeholder=""/>
+                     <input name='email' type="email" className="form-control border-dark" value={email} 
+                     id="email" placeholder="" onChange={(e) => {onInputChange(e)}}/>
                    </div>
                 </div> 
                 <div className="row mb-3">
                    <label htmlFor="status" className="col-sm-4 col-form-label"><b>Marital Status:</b></label>
                    <div className="col-sm-7">
                      <div className="form-check form-check-inline">
-                       <input className="form-check-input" type="radio" name="gender" id="married" value="married" />
+                       <input  className="form-check-input" type="radio" name="marital_status" id="married" 
+                       value={marital_status} onChange={(e) => {onInputChange(e)}}/>
                        <label className="form-check-label" htmlFor="married">Married</label>
                      </div>
                      <div className="form-check form-check-inline">
-                       <input className="form-check-input" type="radio" name="status" id="unmarried" value="unmarried" />
+                       <input className="form-check-input" type="radio" name="marital_status" id="unmarried"
+                        value={marital_status} onChange={(e) => {onInputChange(e)}}/>
                        <label className="form-check-label" htmlFor="unmarried">Unmarried</label>
                      </div>
-                     <div className="form-check form-check-inline">
-                       <input className="form-check-input" type="radio" name="status" id="other" value="other" />
+                     {/* <div className="form-check form-check-inline">
+                       <input className="form-check-input" type="radio" name="marital_status" id="other" 
+                       value={marital_status} onChange={(e) => {onInputChange(e)}}/>
                        <label className="form-check-label" htmlFor="other">Other</label>
-                     </div>
+                     </div> */}
                    </div>
                 </div> 
                 <div className="row mb-3">
                    <label htmlFor="address" className="col-sm-4 col-form-label"><b>Address:</b></label>
                    <div className="col-sm-7">
-                     <input type="address" className="form-control border-dark" id="address" placeholder=""/>
+                     <input name='address' type="address" className="form-control border-dark" 
+                     value={address} id="address" placeholder="" onChange={(e) => {onInputChange(e)}}/>
                    </div>
                 </div>   
                 <div className="row mb-3">
                    <label htmlFor="state" className="col-sm-4 col-form-label"><b>State:</b></label>
                    <div className="col-sm-7">
-                     <input type="state" className="form-control border-dark" id="state" placeholder=""/>
+                     <input name='state' type="state" className="form-control border-dark" value={state}
+                      id="state" placeholder="" onChange={(e) => {onInputChange(e)}}/>
                    </div>
                 </div>   
                 <div className="row mb-3">
                    <label htmlFor="city" className="col-sm-4 col-form-label"><b>City:</b></label>
                    <div className="col-sm-7">
-                     <input type="city" className="form-control border-dark" id="city" placeholder=""/>
+                     <input name='city' type="city" className="form-control border-dark" value={city}
+                      id="city" placeholder="" onChange={(e) => {onInputChange(e)}}/>
                    </div>
                 </div>   
                 <div className="row mb-3">
                    <label htmlFor="pin" className="col-sm-4 col-form-label"><b>PinCode:</b></label>
                    <div className="col-sm-7">
-                     <input type="number" className="form-control border-dark" id="pin" placeholder=""/>
+                     <input name='pincode' type="number" className="form-control border-dark" value={pincode}
+                      id="pin" placeholder="" onChange={(e) => {onInputChange(e)}}/>
                    </div>
                 </div>   
-                <Link className="btn btn-dark float-end" to={"/additionalDetails"}>Next Page</Link>
+                <Link onClick={onClick} className="btn btn-dark float-end" to={"/additionalDetails"}>Next Page</Link>
                 </form>
             </div>
         </div>       
--- bank-management-system/src/SignUpPage/WelcomPage.js
+++ bank-management-system/src/SignUpPage/WelcomPage.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bank_icon from '../Images/bank-icon.jpg';
 import { Link } from 'react-router-dom';
 
+
 export default function WelcomPage() {
+  const[msg, setMsg] = useState();
+
+  const handleChange = (e)=>{
+    setMsg(e.target.value);
+  }
+
+  const handleClick = () =>{
+    setMsg('');
+  }
+
   return (
     <div className='container'>
         <div className="row">
@@ -13,18 +24,18 @@ export default function WelcomPage() {
                 <div className="row mb-4">
                    <label for="Number" className="col-sm-4 col-form-label"><b>Card No:</b></label>
                    <div className="col-sm-7">
-                     <input type="email" className="form-control border-dark" id="Number" placeholder=""/>
+                     <input type="number" onChange={handleChange} className="form-control border-dark" id="Number" placeholder=""/>
                    </div>
                 </div>
                 <div className="row mb-3">
                    <label for="pin" className="col-sm-4 col-form-label"><b>PIN:</b></label>
                    <div className="col-sm-7">
-                     <input type="password" className="form-control border-dark" id="pin" placeholder=""/>
+                     <input type="password" onChange={handleChange} className="form-control border-dark" id="pin" placeholder=""/>
                    </div>
                 </div> <br/>
                 <div class="d-grid gap-2 d-md-block">
                     <button type="submit" className="btn btn-outline-dark">Sign In</button>
-                    <button type="submit" className="btn btn-outline-dark mx-2 "> Cancel </button>
+                    <button onClick={handleClick} className="btn btn-outline-dark mx-2 "> Clear </button>
                 </div><br></br>
                 <div className="d-grid gap-2 col-sm-6 mx-auto">
                 <Link type="submit" className="btn btn-outline-dark mx-2" to={"/signup"}>Sign Up</Link>
